c3b715ad9a6b08a7eadb15e3fd619522ebabc247
n-pretorious <pretoriousndungu@gmail.com>
2020-10-13 11:38:59 +0300

    edit settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -57,8 +57,6 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
